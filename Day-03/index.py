e5cf8985e9da257dbdc9fb1636d13062f937c18e
Sumbal Naz <Codeq209@gmail.com>
2025-03-03 21:11:03 +0500

    Update index.py

--- Day-03/index.py
+++ Day-03/index.py
@@ -1,3 +1,19 @@
+# Write a Python program that takes a number from the user and checks whether it is even
+num = int(input("Enter a Number: "))
+if num % 2 == 0:
+    print(f"{num} is an Even number")
+else:
+    print(f"{num} is not an Even number")
+
+# Write a Python program that takes a number from the user and checks whether it is odd
+num = int(input("Enter a Number: "))
+
+if num % 2 != 0:
+    print(f"{num} is an Odd number")
+else:
+    print(f"{num} is not an Odd number")
+
+#Write a Python program that takes a number from the user and checks whether it is a prime number or not!
 def prime(n):
     if n < 2:
         return False
